a04af5518108ef58adff72a77f0d5205051a860b
FFbsilva <35637163+FFbsilva@users.noreply.github.com>
2021-02-25 13:41:59 -0300

    Fim da correção de fonte no Display

--- CalculadoraJava/src/br/com/felipeb/calc/view/Display.java
+++ CalculadoraJava/src/br/com/felipeb/calc/view/Display.java
@@ -2,6 +2,8 @@ package br.com.felipeb.calc.view;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -12,10 +14,11 @@ public class Display extends JPanel{
 	
 	public Display() {
 		label = new JLabel("Abiguinho");
-		setLayout(new BorderLayout());
+		setLayout(new FlowLayout(FlowLayout.RIGHT,10,25));
 		setBackground(new Color(46,49,50));
 		label.setForeground(Color.white);
-		add(label,BorderLayout.EAST);
+		label.setFont(new Font("courier", Font.PLAIN, 30));
+		add(label);
 	}
 	
 	
