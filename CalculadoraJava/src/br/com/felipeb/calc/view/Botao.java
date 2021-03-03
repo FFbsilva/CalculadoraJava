17864f38404c3a825c807148e451698570a459cb
FFbsilva <35637163+FFbsilva@users.noreply.github.com>
2021-03-03 12:44:31 -0300

    Personalizando os botões da Calculadora

--- CalculadoraJava/src/br/com/felipeb/calc/view/Botao.java
+++ CalculadoraJava/src/br/com/felipeb/calc/view/Botao.java
@@ -1,8 +1,23 @@
 package br.com.felipeb.calc.view;
 
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 
 @SuppressWarnings("serial")
 public class Botao extends JButton{
 
+	public Botao(String texto, Color cor) {
+		
+		setText(texto);
+		setBackground(cor);
+		setOpaque(true);
+		setForeground(Color.WHITE);
+		setFont(new Font("courier", Font.PLAIN, 19));
+		setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		
+		
+	}
 }
--- CalculadoraJava/src/br/com/felipeb/calc/view/Teclado.java
+++ CalculadoraJava/src/br/com/felipeb/calc/view/Teclado.java
@@ -1,18 +1,49 @@
 package br.com.felipeb.calc.view;
 
 import java.awt.Color;
-import java.util.ArrayList;
-import java.util.List;
+import java.awt.GridLayout;
 
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Teclado extends JPanel {
 
+	private final Color COR_CINZA = new Color(68,68,68);
+	private final Color COR_LARANJA = new Color(242,163,60);
+	private final Color COR_CLARO = new Color(98,98,98);
+	
 	public Teclado() {
+		
 		setBackground(Color.BLACK);
+		setLayout(new GridLayout(5,4));
+		add(new Botao("AC", COR_CINZA));
+		add(new Botao("+-", COR_CINZA));
+		add(new Botao("%", COR_CINZA));
+		add(new Botao("/", COR_LARANJA));
+		
+		add(new Botao("7", COR_CLARO));
+		add(new Botao("8", COR_CLARO));
+		add(new Botao("9", COR_CLARO));
+		add(new Botao("*", COR_LARANJA));
+		
+		add(new Botao("7", COR_CLARO));
+		add(new Botao("8", COR_CLARO));
+		add(new Botao("9", COR_CLARO));
+		add(new Botao("*", COR_LARANJA));
+		
+		add(new Botao("7", COR_CLARO));
+		add(new Botao("8", COR_CLARO));
+		add(new Botao("9", COR_CLARO));
+		add(new Botao("*", COR_LARANJA));
+		
+		add(new Botao("7", COR_CLARO));
+		add(new Botao("8", COR_CLARO));
+		add(new Botao("9", COR_CLARO));
+		add(new Botao("*", COR_LARANJA));
+		
+		
 	}
 	
-	List<Botao> botoes = new ArrayList<>();
+	
 	
 }
